f20ba79afa95e10b4cabac7e8291ffb321b6d382
Santiago Mena <santiagomenape@gmail.com>
2023-07-29 04:48:02 +0100

    Plugin Routes + Menu

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,12 @@
 namespace santiagomena\craftbookshelf;
 
 use Craft;
+use craft\base\Event;
 use craft\base\Plugin as BasePlugin;
+use craft\events\RegisterCpNavItemsEvent;
+use craft\events\RegisterUrlRulesEvent;
+use craft\web\twig\variables\Cp;
+use craft\web\UrlManager;
 
 /**
  * bookshelf plugin
@@ -37,6 +42,35 @@ class Plugin extends BasePlugin
     private function attachEventHandlers(): void
     {
         // Register event handlers here ...
-        // (see https://craftcms.com/docs/4.x/extend/events.html to get started)
+        // Routes
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function(RegisterUrlRulesEvent $event) {
+                $event->rules['bookshelf'] = '_bookshelf/book/index';
+            }
+        );
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function(RegisterUrlRulesEvent $event) {
+                $event->rules['bookshelf'] = '_bookshelf/book/book';
+            }
+        );
+        // Menu item
+
+        // Register the sidebar menu item
+        Event::on(
+            Cp::class,
+            Cp::EVENT_REGISTER_CP_NAV_ITEMS,
+            function(RegisterCpNavItemsEvent $event) {
+                $event->navItems[] = [
+                    'url' => 'bookshelf',
+                    'label' => 'Bookshelf',
+                    'icon' => '@santiagomena/craftbookshelf/icon.svg',
+                ];
+            }
+        );
     }
 }
