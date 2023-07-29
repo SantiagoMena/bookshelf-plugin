60b41659d8456f9a4c4d227e6ca89cbc00b7ba12
Santiago Mena <santiagomenape@gmail.com>
2023-07-29 10:48:30 +0100

    Share Public Books

--- src/controllers/BookController.php
+++ src/controllers/BookController.php
@@ -2,14 +2,35 @@
 namespace santiagomena\craftbookshelf\controllers;
 
 use Craft;
+use craft\db\Query;
+use craft\db\Table;
+use craft\elements\User;
+use craft\helpers\Db;
+use craft\models\EntryType;
 use craft\web\Controller;
+use craft\web\View;
+
 
 class BookController extends Controller
 {
-
     public function actionIndex()
     {
-        echo "INDEX";
+        return $this->renderTemplate('_bookshelf/book/index');
+    }
+
+    public function actionPublic(string $username)
+    {
+        $author = User::find()->where(['username' => $username])->one();
+
+        $books = [];
+        if($author) {
+            $books = \craft\elements\Entry::find()->section('bookshelfBookSection')->authorId($author->id);
+        }
+
+        Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
+        return Craft::$app->view->renderTemplate('_bookshelf/book/indexPublic', [
+            'books' => $books
+        ]);
     }
 
     public function actionAdd()
